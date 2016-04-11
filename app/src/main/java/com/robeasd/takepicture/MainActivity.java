34d3e479289e5abbe0878a7de389f2df557122cd
Roberasd <rovalos@gmail.com>
2016-04-11 11:02:09 -0500

    Agregados permisos para Android 6

--- app/src/main/java/com/robeasd/takepicture/MainActivity.java
+++ app/src/main/java/com/robeasd/takepicture/MainActivity.java
@@ -1,35 +1,46 @@
 package com.robeasd.takepicture;
 
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.os.PersistableBundle;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 
-
-public class MainActivity extends ActionBarActivity {
-
-    private String APP_DIRECTORY = "myPictureApp/";
-    private String MEDIA_DIRECTORY = APP_DIRECTORY + "media";
-    private String TEMPORAL_PICTURE_NAME = "temporal.jpg";
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 
+public class MainActivity extends AppCompatActivity {
+
+    private String APP_DIRECTORY = "MyPictureApp/";
+    private String MEDIA_DIRECTORY = APP_DIRECTORY + "PicturesApp";
+    private final int MY_WRITE_EXTENERNAL_STORAGE_PERMISSION = 100;
 
     private final int PHOTO_CODE = 100;
     private final int SELECT_PICTURE = 200;
 
     private ImageView imageView;
+    private Button mOptionButton;
+    private String mPath;
 
 
     @Override
@@ -37,47 +48,86 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView) findViewById(R.id.setPicture);
-        Button button = (Button) findViewById(R.id.buttonImage);
+        imageView = (ImageView) findViewById(R.id.set_picture);
+        mOptionButton = (Button) findViewById(R.id.show_options_button);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        if (mayRequestStoragePermission()){
+
+        }
+
+        mOptionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final CharSequence[] options = {"Tomar foto", "Elegir de galeria", "Cancelar"};
-                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("Elige una opcion :D");
-                builder.setItems(options, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int seleccion) {
-                        if(options[seleccion] == "Tomar foto"){
-                            openCamera();
-                        }else if (options[seleccion] == "Elegir de galeria") {
-                            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                            intent.setType("image/*");
-                            startActivityForResult(intent.createChooser(intent, "Selecciona app de imagen"), SELECT_PICTURE);
-                        }else if(options[seleccion] == "Cancelar"){
-                            dialog.dismiss();
-                        }
-                    }
-                });
-                builder.show();
+                showOptions();
             }
         });
+    }
 
+    private void showOptions() {
+        final CharSequence[] options = {"Tomar foto", "Elegir de galeria", "Cancelar"};
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Elige una opcion :D");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int seleccion) {
+                if(options[seleccion] == "Tomar foto"){
+                    openCamera();
+                }else if (options[seleccion] == "Elegir de galeria") {
+                    Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(intent.createChooser(intent, "Selecciona app de imagen"), SELECT_PICTURE);
+                }else if(options[seleccion] == "Cancelar"){
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private boolean mayRequestStoragePermission(){
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+
+        if ((checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) && (checkSelfPermission(CAMERA) == PackageManager.PERMISSION_GRANTED)) {
+            return true;
+        }
+
+        if ((shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) || (shouldShowRequestPermissionRationale(CAMERA))) {
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA},
+                    MY_WRITE_EXTENERNAL_STORAGE_PERMISSION);
+
+        } else {
+            requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA},
+                    MY_WRITE_EXTENERNAL_STORAGE_PERMISSION);
+        }
+
+        return false;
     }
 
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("file_path", mPath);
+    }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
 
+        mPath = savedInstanceState.getString("file_path");
+    }
 
     private void openCamera() {
         File file = new File(Environment.getExternalStorageDirectory(), MEDIA_DIRECTORY);
         file.mkdirs();
+        Long tsLong = System.currentTimeMillis()/1000;
+        String ts = tsLong.toString() + ".jpg";
+        mPath = Environment.getExternalStorageDirectory() + File.separator
+                + MEDIA_DIRECTORY + File.separator + ts;
 
-        String path = Environment.getExternalStorageDirectory() + File.separator
-                + MEDIA_DIRECTORY + File.separator + TEMPORAL_PICTURE_NAME;
-
-        File newFile = new File(path);
+        File newFile = new File(mPath);
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newFile));
@@ -91,9 +141,8 @@ public class MainActivity extends ActionBarActivity {
         switch (requestCode){
             case PHOTO_CODE:
                 if(resultCode == RESULT_OK){
-                  String dir =  Environment.getExternalStorageDirectory() + File.separator
-                          + MEDIA_DIRECTORY + File.separator + TEMPORAL_PICTURE_NAME;
-                    decodeBitmap(dir);
+
+                    decodeBitmap(mPath);
                 }
             break;
 
@@ -105,6 +154,43 @@ public class MainActivity extends ActionBarActivity {
             break;
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_WRITE_EXTENERNAL_STORAGE_PERMISSION) {
+            if (grantResults.length == 2 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+            }else{
+                showExplanation();
+            }
+        }
+    }
+
+
+    private void showExplanation() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Permisos denegados");
+        builder.setMessage("Para usar esta aplicacion se necesita que des permisos para poder usar" +
+                "la camara y para poder guardar las imagenes en memoria.");
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent();
+                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
     }
 
     private void decodeBitmap(String dir) {
